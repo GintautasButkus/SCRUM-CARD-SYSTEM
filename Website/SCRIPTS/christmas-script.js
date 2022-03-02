04c3e1615caee7a8ce4797dd8188e09314bfc803
Antanas Kastravickas <Antanas.Kastravickas@gmail.com>
2022-03-02 16:21:40 +0200

    added functionality to download

--- Website/SCRIPTS/christmas-script.js
+++ Website/SCRIPTS/christmas-script.js
@@ -59,9 +59,38 @@ function changeColor() {
     backgroundIm.style.backgroundColor = color;
 }
 
+// *************Download postcard************************************* 
+
+function downloadtable() {
+
+    var node = document.getElementById('tablecontainer');
+
+    domtoimage.toPng(node)
+        .then(function (dataUrl) {
+            var img = new Image();
+            img.src = dataUrl;
+            downloadURI(dataUrl, "records.png")
+        })
+        .catch(function (error) {
+            console.error('oops, something went wrong', error);
+        });
+}
+
+function downloadURI(uri, name) {
+    var link = document.createElement("a");
+    link.download = name;
+    link.href = uri;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    delete link;
+}
+
 
 
 // Christmas own decoration
+
+
 
 function previewFile() {
     const preview = document.getElementById('img-area');
